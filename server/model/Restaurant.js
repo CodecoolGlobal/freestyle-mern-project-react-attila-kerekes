d44626ebd8771dbccf252a056efa9ef67bb72ac7
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2023-10-03 10:25:37 +0200

    Creating the restaurant model

--- server/model/Restaurant.js
+++ server/model/Restaurant.js
@@ -1,8 +1,39 @@
 import mongoose from "mongoose";
-const {Schema, model} = mongoose;
+const { Schema, model } = mongoose;
 
 const restaurantSchema = new Schema({
-    
-})
+  restaurantName: {
+    type: String,
+    required: true
+  },
+  address: {
+    country: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    locationNumber: {
+        type: Number,
+        required: true,
+      }
+  },
+  opening: String,
+  closing: String,
+  email: {
+    type: String,
+    required
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  }
+});
 
-export default model('Restaurant', restaurantSchema);
+export default model("Restaurant", restaurantSchema);
