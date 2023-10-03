70e3f9c671b9e583b70078e9b251248134079d45
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2023-10-03 10:05:01 +0200

    Changing the resorvation model

--- server/model/Restaurant.js
+++ server/model/Restaurant.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose";
 const {Schema, model} = mongoose;
 
 const restaurantSchema = new Schema({
-    restaurantId: String,
-    customerId: String,
-    numberOfGuests: Number,
-    tablesBooked: ?,
-    createdAt: Date,
-    updatedAt: Date
+    
 })
 
 export default model('Restaurant', restaurantSchema);
