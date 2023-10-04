0f9bd38403ecffceb578df93c10de7bb77ebbc85
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2023-10-04 14:18:06 +0200

    Finishing the my restaurant page

--- client/src/pages/MyRestaurant.jsx
+++ client/src/pages/MyRestaurant.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Outlet, useParams } from "react-router-dom";
 import RestaurantNav from "../components/RestaurantNav";
+import RestaurantTableElements from '../components/RestaurantTableElements';
 
 
 function RestaurantMain(){
@@ -22,8 +23,26 @@ function RestaurantMain(){
     return(
         <div className="restaurantMain">
             <RestaurantNav />
+            {restaurant && 
+                <div className="my-restaurant">
+                    <p>Restaurant name: {restaurant.restaurantName}</p>
+                    <p>Opening: {restaurant.opening}</p>
+                    <p>Closing: {restaurant.closing}</p>
+                    <p>Email address: {restaurant.email}</p>
+                    <p>Phone number: {restaurant.phoneNumber}</p>
+                    <p>Tables: {restaurant.tables.length ? restaurant.tables.length : 'You don\'t have any tables!'}</p>
+                    <table>{restaurant.tables.map(table => <RestaurantTableElements key={table.id} table={table}/>)}</table>
+                </div>
+            }
         </div>
     )
 }
 
 export default RestaurantMain;
+
+
+/*
+    id
+    székek
+    foglalható
+*/
